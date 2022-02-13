48a0adec0bce790e5d77e61353f97085d210a1c3
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2022-02-13 11:51:11 +0530

    #include<stdio.h> int main() {     int a,b,t=2,lcm=1;     scanf("%d%d",&a,&b);     while(a>=t && b>=t)     {         if(a%t==0 && b%t==0)         {             a=a/t;             b=b/t;             lcm=lcm*t;         }         else         {             t++;         }     }     printf("%d",a*b*lcm); }

---
+++ LCM.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,t=2,lcm=1;
+    scanf("%d%d",&a,&b);
+    while(a>=t && b>=t)
+    {
+        if(a%t==0 && b%t==0)
+        {
+            a=a/t;
+            b=b/t;
+            lcm=lcm*t;
+        }
+        else
+        {
+            t++;
+        }
+    }
+    printf("%d",a*b*lcm);
+}
