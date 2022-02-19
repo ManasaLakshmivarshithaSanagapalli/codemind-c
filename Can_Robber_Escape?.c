a8b097e5a560981ffa104c208d216e6b7ffedc56
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2022-02-19 21:47:04 +0530

    #include<stdio.h> int main() {     int n,arr[100],odd=0,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             odd++;         }     }     if(odd<=2)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],odd=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            odd++;
+        }
+    }
+    if(odd<=2)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
