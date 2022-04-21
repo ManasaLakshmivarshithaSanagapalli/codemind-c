5ada8ef8456366b0441c6109ccc7a5d10b1c2fa7
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2022-04-21 23:13:37 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,p;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2)         {             p=arr[i];         }     }     printf("%d",p); }

---
+++ last_odd_number_in_an_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,p;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2)
+        {
+            p=arr[i];
+        }
+    }
+    printf("%d",p);
+}
