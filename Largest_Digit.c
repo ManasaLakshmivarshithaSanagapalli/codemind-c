9606fda908edb42afefe720f5239e8e2292e0b21
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2022-04-22 10:38:08 +0530

    #include<stdio.h> int main() {     int n,m=0,d;     scanf("%d",&n);     while(n!=0)     {         d=n%10;         n=n/10;     if(d>m)     {         m=d;     }     }     printf("%d",m); }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,d,m;
+    int n,m=0,d;
     scanf("%d",&n);
-    while(n)
+    while(n!=0)
     {
         d=n%10;
         n=n/10;
-        if(d>m)
-        {
-            m=d;
-        }
-        else
-        {
-            continue;
-        }
+    if(d>m)
+    {
+        m=d;
+    }
     }
     printf("%d",m);
 }
