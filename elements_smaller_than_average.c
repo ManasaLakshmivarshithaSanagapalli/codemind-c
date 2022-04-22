11d064623ed57b3674ccabd12957183383c4c191
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2022-04-22 18:03:33 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,sum=0,avg,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum+=arr[i];     }     avg=sum/n;     for(i=0;i<n;i++)     {         if(arr[i]<=avg)         {             c++;         }     }     printf("%d",c); }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,sum=0,avg,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    avg=sum/n;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<=avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
