12f8cd3f6ab7633ebfe8bb340f7919ef9f42eb1c
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2022-05-24 18:03:01 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],m,k,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&m,&k);     for(i=0;i<n;i++)     {         if(arr[i]>=m && arr[i]<=k||arr[i]<=m &&arr[i]>=k)         {             printf("%d ",arr[i]);             c++;         }     }     if(c==0)     {         printf("-1");     } }

---
+++ elements_in_between_A_and_B.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],m,k,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&m,&k);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=m && arr[i]<=k||arr[i]<=m &&arr[i]>=k)
+        {
+            printf("%d ",arr[i]);
+            c++;
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+}
