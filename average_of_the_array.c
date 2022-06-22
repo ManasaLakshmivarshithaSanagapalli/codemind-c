7f062eb65a01c00074a4cb2542e511e84baa772c
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2022-06-22 11:38:58 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],sum=0;     float avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     avg=(float)sum/n;     printf("%.2f",avg); }

--- average_of_the_array.c
+++ average_of_the_array.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i,sum=0;
+    int n,i,arr[100],sum=0;
     float avg;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-    scanf("%d",&arr[i]);
+        scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
     {
-        sum+=arr[i];
+        sum=sum+arr[i];
     }
     avg=(float)sum/n;
     printf("%.2f",avg);
