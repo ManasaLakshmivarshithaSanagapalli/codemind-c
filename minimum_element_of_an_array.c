c45f0e7ac91ea891209bc8681ca204ba22744ff2
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2022-06-27 23:13:54 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],m=1000;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]<m)         {             m=arr[i];         }     }     printf("%d",m); }

--- minimum_element_of_an_array.c
+++ minimum_element_of_an_array.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i,m;
+    int n,i,arr[100],m=1000;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    m=arr[0];
     for(i=0;i<n;i++)
     {
         if(arr[i]<m)
