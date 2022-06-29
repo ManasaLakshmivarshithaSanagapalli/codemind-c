6bf5762cde57a679d7042e26f5dbd2e0e3976f32
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2022-06-29 18:58:38 +0530

    #include<stdio.h> int digit_count(int n) {     int i,dc=0,d;     while(n)     {         d=n%10;         n=n/10;         dc++;     }     return dc; } int main() {     int n,i,arr[100],p,dc=0,min=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         dc=0;         p=digit_count(arr[i]);         if(min<p)         {             min=p;         }     }     for(i=0;i<n;i++)     {         dc=0;         p=digit_count(arr[i]);         if((digit_count(arr[i]))==min)         {             c++;         }     }     printf("%d",c); }

--- Max_digits_count.c
+++ Max_digits_count.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int digit_count(int n)
 {
-    int dc=0,i,d;
-        while(n!=0)
-        {
-            d=n%10;
-            n=n/10;
-            dc++;
-        }
+    int i,dc=0,d;
+    while(n)
+    {
+        d=n%10;
+        n=n/10;
+        dc++;
+    }
     return dc;
 }
 int main()
 {
-    int n,i,arr[100],max=0,dc=0,p,c=0;
+    int n,i,arr[100],p,dc=0,min=0,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -22,14 +22,16 @@ int main()
     {
         dc=0;
         p=digit_count(arr[i]);
-        if(max<p)
+        if(min<p)
         {
-            max=p;
+            min=p;
         }
     }
     for(i=0;i<n;i++)
     {
-        if(digit_count(arr[i])==max)
+        dc=0;
+        p=digit_count(arr[i]);
+        if((digit_count(arr[i]))==min)
         {
             c++;
         }
